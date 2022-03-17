2c377fe11387c92457b505896413cd6068a64ff7
ImDev11 <euris110811@outlook.com>
2022-03-17 16:00:57 -0400

    Feat: DataContext for connection to DB

---
+++ src/Controllers/DataContext/DataContext.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controllers.DataContext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author euris
+ */
+public class DataContext {
+    private final String database = "sistema_facturacion";
+    private final String user = "postgres";
+    private final String password = "Immnqwerty071102";
+    private final String host = "localhost";
+    private final String port = "5432";
+    private final String driver = "org.postgresql.Driver";
+    private final String connectionString = "jdbc:postgresql://" + host + ":" + port +"/" + database;
+    
+    public Connection connect() {
+        Connection connection = null;
+        
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(connectionString, user, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DataContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return connection;
+    }
+}
