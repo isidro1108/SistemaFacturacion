bd00b28da8b090eebba3e32e3017e318803ae29f
ImDev11 <euris110811@outlook.com>
2022-03-19 00:11:51 -0400

    Constants class created

---
+++ src/Views/Constants/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Views.Constants;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author euris
+ */
+public class Constants {
+    public class Colors{
+        public static Color LIGHT_BLUE = new java.awt.Color(200, 255, 255);
+        public static Color DARK_BLUE = new java.awt.Color(0, 0, 255);
+    }
+}
