a55dccadba48f0a9f1f04c1674f61600c00beaae
ImDev11 <euris110811@gmail.com>
2022-04-28 07:07:52 +0100

    Feat: Customer registration done, files added

---
+++ src/Controllers/ClienteController.java
@@ -0,0 +1,149 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controllers;
+
+import Controllers.DataContext.DataContext;
+import Controllers.Interfaces.IClienteController;
+import Entities.Cliente;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author euris
+ */
+public class ClienteController implements IClienteController {
+    private final DataContext dbContext;
+
+    public ClienteController() {
+        this.dbContext = new DataContext();
+    }
+    
+    @Override
+    public List<Cliente> getAll() {
+        Connection connection = dbContext.connect();
+        List<Cliente> clientes = new ArrayList<>();
+        String sql = "SELECT * FROM customer ORDER BY id ASC";
+        
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            
+            while (result.next()) {
+                Cliente cliente = new Cliente();
+                
+                cliente.setId(result.getInt("id"));
+                cliente.setName(result.getString("name"));
+                cliente.setLastName(result.getString("last_name"));
+                cliente.setAddress(result.getString("address"));
+                cliente.setPhone(result.getString("phone"));
+                cliente.setEmail(result.getString("email"));
+                cliente.setDateOfBirth(result.getDate("date_of_birth"));
+                cliente.setCreditLimit(result.getFloat("credit_limit"));
+                clientes.add(cliente);
+            }
+            connection.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return clientes;
+    }
+
+    @Override
+    public Cliente getById(int id) {
+        Connection connection = dbContext.connect();
+        Cliente cliente = new Cliente();
+        String sql = "SELECT * FROM customer ORDER BY id ASC";
+        
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            
+            if (result.next()) {                
+                cliente.setId(result.getInt("id"));
+                cliente.setName(result.getString("name"));
+                cliente.setLastName(result.getString("last_name"));
+                cliente.setAddress(result.getString("address"));
+                cliente.setPhone(result.getString("phone"));
+                cliente.setEmail(result.getString("email"));
+                cliente.setDateOfBirth(result.getDate("date_of_birth"));
+                cliente.setCreditLimit(result.getFloat("credit_limit"));
+            } else {
+                JOptionPane.showMessageDialog(null, "Este cliente no existe", "Warning", JOptionPane.WARNING_MESSAGE);
+            }
+            connection.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return cliente;
+    }
+
+    @Override
+    public int create(Cliente cliente) {
+        Connection connection = dbContext.connect();
+        String sql = "INSERT INTO customer (name, last_name, address, phone, email, date_of_birth, credit_limit)"
+                + "VALUES ('"+ cliente.getName() +"', "
+                + "'"+ cliente.getLastName()+"', "
+                + "'"+ cliente.getAddress()+"', "
+                + "'"+ cliente.getPhone() +"', "
+                + "'"+ cliente.getEmail()+"', "
+                + "'"+ cliente.getDateOfBirth()+"', "
+                + "'"+ cliente.getCreditLimit()+"') RETURNING id";
+        
+        int idCustomer = 0;
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            
+            if (result.next()) idCustomer = result.getInt("id");
+            connection.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return idCustomer;
+    }
+
+    @Override
+    public void update(Cliente cliente) {
+        Connection connection = dbContext.connect();
+        
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "UPDATE customer SET "
+                    + "name='" + cliente.getName() + "', "
+                    + "last_name='" + cliente.getLastName()+ "', "
+                    + "address='" + cliente.getAddress()+ "', "
+                    + "phone='" + cliente.getPhone() + "', "
+                    + "email='" + cliente.getEmail()+ "', "
+                    + "date_of_birth='" + cliente.getDateOfBirth()+ "', "
+                    + "credit_limit=" + cliente.getCreditLimit()
+                    + " WHERE id=" + cliente.getId();
+            
+            statement.execute(sql);
+            connection.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        Connection connection = dbContext.connect();
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "DELETE FROM customer WHERE id=" + id;
+            statement.execute(sql);
+            connection.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+}
---
+++ src/Entities/Cliente.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author euris
+ */
+public class Cliente {
+    private int id;
+    private String name;
+    private String lastName;
+    private String address;
+    private String phone;
+    private String email;
+    private Date dateOfBirth;
+    private float creditLimit;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public float getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(float creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+}
